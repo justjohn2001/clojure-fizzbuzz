074b1e0d7ae62aa94e9702c392bdf044bbf317e5
John <john@onejohn.org>
2020-07-06 11:21:05 -0400

    add lazy version

--- src/fizzbuzz/core.clj
+++ src/fizzbuzz/core.clj
@@ -1,11 +1,11 @@
 (ns fizzbuzz.core
-  (:gen-class))
+  (:require [clojure.string :as string]))
 
 (defn make-fb
   [[k s]]
   (map (fn [[n fs :as p]]
          (if (zero? (mod n k))
-           (vector n ((fnil str "") fs s))
+           (vector n (str fs s))
            p))))
 
 (defn fb-transducer
@@ -15,9 +15,28 @@
                 (sort-by first m))
         (map (fn [[n s]] (or s n)))))
 
+(defn fb-fn
+  [m]
+  (fn [n]
+    (or (reduce (fn [v [k s]]
+                  (if (zero? (mod n k))
+                    (str v s)
+                    v))
+                nil
+                m)
+        n)))
+
+(def fizz-buzz (fb-fn {3 "fizz" 5 "buzz" 7 "bazz"}))
+
+(defn lazy-fb
+  ([] (lazy-fb 1))
+  ([n] (cons (fizz-buzz n)
+             (lazy-seq (lazy-fb (inc n))))))
+
 (defn -main
   [& args]
-  (println
-   (clojure.string/join ", "
+  (println (string/join ", "
                         (sequence (fb-transducer {3 "fizz" 5 "buzz" 7 "baz"})
-                                  (range 1 20)))))
+                                  (range 1 20))))
+  (println (string/join ", "
+                        (take 20 (lazy-fb)))))
